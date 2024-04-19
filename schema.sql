e5cf57cf2cf5d6c459edc92dcfe4cdd15198f927
G4BRI3L3 <71242816@itiscastelli.onmicrosoft.com>
2024-04-19 23:14:22 +0200

    eliminato file inutili

--- schema.sql
+++ schema.sql
@@ -1,50 +1,20 @@
--- SQLite
-CREATE TABLE Utente (
+-- schema.sql
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS recipe;
+
+CREATE TABLE user (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL UNIQUE,
-    email TEXT NOT NULL UNIQUE,
+    username TEXT UNIQUE NOT NULL,
+    email TEXT UNIQUE NOT NULL,
     password_hash TEXT NOT NULL
 );
 
-CREATE TABLE Ricetta (
+CREATE TABLE recipe (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    titolo TEXT NOT NULL,
-    descrizione TEXT NOT NULL,
-    procedimento TEXT NOT NULL,
-    data_pubblicazione DATETIME DEFAULT CURRENT_TIMESTAMP,
-    id_utente INTEGER NOT NULL,
-    FOREIGN KEY (id_utente) REFERENCES Utente (id)
-);
-
-CREATE TABLE Ingrediente (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nome TEXT NOT NULL,
-    quantita TEXT NOT NULL
-);
-
-CREATE TABLE Ricetta_Ingrediente (
-    id_ricetta INTEGER,
-    id_ingrediente INTEGER,
-    PRIMARY KEY (id_ricetta, id_ingrediente),
-    FOREIGN KEY (id_ricetta) REFERENCES Ricetta (id),
-    FOREIGN KEY (id_ingrediente) REFERENCES Ingrediente (id)
-);
-
-CREATE TABLE Voto (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    punteggio INTEGER CHECK(punteggio >= 1 AND punteggio <= 5),
-    id_ricetta INTEGER,
-    id_utente INTEGER,
-    UNIQUE (id_ricetta, id_utente),
-    FOREIGN KEY (id_ricetta) REFERENCES Ricetta (id),
-    FOREIGN KEY (id_utente) REFERENCES Utente (id)
-);
-
-CREATE TABLE Recensione (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    testo TEXT NOT NULL,
-    id_ricetta INTEGER,
-    id_utente INTEGER,
-    FOREIGN KEY (id_ricetta) REFERENCES Ricetta (id),
-    FOREIGN KEY (id_utente) REFERENCES Utente (id)
+    name TEXT NOT NULL,
+    ingredients TEXT NOT NULL,
+    procedure TEXT NOT NULL,
+    star_rating REAL DEFAULT 0,
+    user_id INTEGER,
+    FOREIGN KEY (user_id) REFERENCES user (id)
 );
